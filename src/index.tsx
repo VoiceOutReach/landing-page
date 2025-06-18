f9a9d4f2927877c6d08814bac3cc2878b97cad73
hassan amjad <hassanamjad@hassans-MacBook-Air.local>
2025-06-19 02:49:32 +0500

    ✅ Fix: Removed unused expression in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Desktop from "./Desktop";
-import ThankYou from "./ThankYou";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Desktop />} />
-        <Route path="/thankyou" element={<ThankYou />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
